30f5be548cd5636651436081be7872cdc7b9ecd6
Oqla Alrefai <2015978054@ses.yu.edu.jo>
2021-11-06 21:54:53 +0200

    finished edit

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# madlib-cli 
-
+# malib_cli
 this is a game computer will ask the user to enter multiple words (adjectives, noun, pronoun, ...) and then it will show funny paragraph
 
 https://github.com/oqlaalrefai/madlib-cli
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -4,20 +4,20 @@ print('=  in this game the compyter will ask you to enter word(adjectives,noun..
 print('=  then youll see text that may be strange and funny                          =')
 print('===============================================================================')
 
-def readFile(path):
+def read_template(path):
     ''' this function read the file '''
     with open(path) as f :
         content = f.read()
     return content
 
     
-def missedWord(path):
+def parse_template(path):
     '''this function return all values inside the curly praces'''
     data=re.findall(r'{(.*?)}',path)
     list= tuple(data)
     replaceddata=re.sub(r'{(.*?)}',"{}",path)
     return replaceddata,list
-print(missedWord('assets/sample.txt'))
+print(parse_template('assets/sample.txt'))
 
 def merge(file , words):  
 
@@ -26,7 +26,7 @@ def merge(file , words):
     """
     testing1=file.format(*words)
     return testing1
-    # lst = missedWord(file)  # file is the parsed text which has text and empty{} to put things inside curly braces in te step below
+    # lst = parse_template(file)  # file is the parsed text which has text and empty{} to put things inside curly braces in te step below
     
     # return (re.sub(r' {[^}]*?}',' {}',file)).format(*words) # we use string.format() to give the string text which have text and empty curly braces values one by one from the values in format 
     #                                         # we put * before the name of array so it will take elements one by one in the array which is (words), then put each value into empty braces
@@ -38,7 +38,7 @@ def writeText (file):
 
 if __name__ == "__main__":
 
-    fileName=readFile("assets/sample.txt")
+    fileName=read_template("assets/sample.txt")
     edited=re.findall(r'{(.*?)}',fileName)
     desiredData=[]
     for i in edited:
